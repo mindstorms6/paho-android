4857f0490e72597f0af9a22c2f1ff9bb8bfc90fa
Breland <breland@bdawg.org>
2013-01-15 12:41:20 -0500

    Configure for Ant builds

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/breland/Documents/GH/paho_android/paho-android/bin/AndroidManifest.xml : \
+/Users/breland/Documents/GH/paho_android/paho-android/AndroidManifest.xml \
---
+++ gen/R.java.d
@@ -0,0 +1,12 @@
+/Users/breland/Documents/GH/paho_android/paho-android/gen/org/bdawg/mobile/mqtt_android/R.java \
+ : /Users/breland/Documents/GH/paho_android/paho-android/res/drawable-hdpi/ic_launcher.png \
+/Users/breland/Documents/GH/paho_android/paho-android/res/drawable-mdpi/ic_launcher.png \
+/Users/breland/Documents/GH/paho_android/paho-android/res/drawable-xhdpi/ic_launcher.png \
+/Users/breland/Documents/GH/paho_android/paho-android/res/values/strings.xml \
+/Users/breland/Documents/GH/paho_android/paho-android/res/values/styles.xml \
+/Users/breland/Documents/GH/paho_android/paho-android/res/values-v11/styles.xml \
+/Users/breland/Documents/GH/paho_android/paho-android/res/values-v14/styles.xml \
+/Users/breland/Documents/GH/paho_android/paho-android/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/breland/Documents/GH/paho_android/paho-android/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/breland/Documents/GH/paho_android/paho-android/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/breland/Documents/GH/paho_android/paho-android/bin/AndroidManifest.xml \
